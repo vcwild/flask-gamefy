deb6ec36583a463fc157b81645d4106be545dbfc
vcwild <vcwildm@gmail.com>
2021-01-06 19:42:03 -0300

    remove mock database

--- lib/games.py
+++
@@ -1,9 +0,0 @@
-from lib.models import Game
-
-games_list = [
-    Game('Killer Instinct', 'Fight', 'Dreamcast'),
-    Game('Mortal Kombat', 'Fight', 'SNES'),
-    Game('Pokémon', 'RPG', 'GBA'),
-    Game('Super Mario', 'Adventure', 'SNES'),
-    Game('Tetris', 'Puzzle', 'NES')
-]
--- lib/users.py
+++
@@ -1,9 +0,0 @@
-from lib.models import User
-
-users = [
-    User('daniel', 'Daniel Hielts', 'DANIEL'),
-    User('john', 'John Doe', 'JOHN'),
-    User('zack', 'Zack Bragda', 'ZACK'),
-    User('flavio', 'Flávio Carlo', 'FLAVIO')
-]
-users_list = {user.id: user for user in users}
