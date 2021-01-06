7d5fcbfcf56e5c9adcb00dd3ab3a5088058358c6
vcwild <vcwildm@gmail.com>
2021-01-06 19:41:05 -0300

    update main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from lib.models import Game, User
-from lib.games import games_list
-from lib.users import users_list
 from data.dao import UserDao, GameDao
 from os import environ
 from flask import (
@@ -8,35 +6,39 @@ from flask import (
 )
 from flask_mysqldb import MySQL
 
+# check if env variables exist
 if environ.get('SECRET_KEY') is None:
     raise KeyError('SECRET_KEY not defined in current environment # export SECRET_KEY=[value]')
 if environ.get('MYSQL_PASSWORD') is None:
     raise KeyError('MYSQL_PASSWORD not defined in environment # export MYSQL_PASSWORD=[value]')
 
-
+# define app
 app = Flask(__name__)
-app.secret_key = environ['SECRET_KEY']
-app.config['MYSQL_HOST'] = "0.0.0.0"
-app.config['MYSQL_USER'] = "user"
-app.config['MYSQL_PASSWORD'] = environ['MYSQL_PASSWORD']
-app.config['MYSQL_DB'] = 'db'
-app.config['MYSQL_PORT'] = 3306
+app.config.update(
+    SECRET_KEY = environ['SECRET_KEY'],
+    MYSQL_HOST = "0.0.0.0",
+    MYSQL_USER = "user",
+    MYSQL_PASSWORD = environ["MYSQL_PASSWORD"],
+    MYSQL_DB = "db",
+    MYSQL_PORT = 3306
+)
 
+# define database
 db = MySQL(app)
-
 game_dao = GameDao(db)
+user_dao = UserDao(db)
 
+# define routes
 @app.route('/')
 def index():
+    games_list = game_dao.list_games()
     return render_template('list.html', title='Games', games=games_list)
-
 
 @app.route('/new')
 def new():
     if 'current_user' not in session or session['current_user'] == None:
         return redirect(url_for('login', next=url_for('new')))
     return render_template('new.html', title='New Game')
-
 
 @app.route('/create', methods=['POST'])
 def create():
@@ -47,17 +49,38 @@ def create():
     game_dao.save(game)
     return redirect(url_for('index'))
 
+@app.route('/edit/<int:id>')
+def edit(id):
+    if 'current_user' not in session or session['current_user'] == None:
+        return redirect(url_for('login', next=url_for('edit')))
+    game = game_dao.search_by_id(id)
+    return render_template('edit.html', title='Edit Game', game=game)
+
+@app.route('/update', methods=['POST'])
+def update():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+    game = Game(name, category, console, id=request.form['id'])
+    game_dao.save(game)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    game_dao.delete_game(id)
+    flash('The game was deleted successfully!')
+    return redirect(url_for('index'))
+    
 
 @app.route('/login')
 def login():
     next = request.args.get(key='next')
     return render_template('login.html', title="Login", next=next)
 
-
 @app.route('/auth', methods=['POST'])
 def auth():
-    if request.form['user'] in users_list:
-        user = users_list[request.form['user']]
+    user = user_dao.search_by_id(request.form['user'])
+    if user:
         if user.password == request.form['password']:
             session['current_user'] = user.id
             flash('Welcome ' + str.capitalize(user.id) + '!')
@@ -67,12 +90,10 @@ def auth():
             flash(u'Invalid user or password!', category='error')
             return redirect(url_for('login'))
 
-
 @app.route('/logout')
 def logout():
     session['current_user'] = None
     flash('User logged out successfully!')
     return redirect(url_for('index'))
-
 
 app.run(debug=True)
